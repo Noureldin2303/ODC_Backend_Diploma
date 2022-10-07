1be03797911699d60eb73759e48ff78fac8ece1b
Noureldin Farag <72710430+Noureldin2303@users.noreply.github.com>
2022-10-07 14:15:41 +0200

    Update index.blade.php

--- Second_Project/personal blog/resources/views/index.blade.php
+++ Second_Project/personal blog/resources/views/index.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -23,8 +22,7 @@
   -->
     <link rel="preconnect" href="{{ url('https://fonts.googleapis.com') }}">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800;900&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/a88e445782.js" crossorigin="anonymous"></script>
     <style>
         .flex-action {
@@ -37,14 +35,16 @@
 
 <body class="light-theme">
     <?php
+
     use Illuminate\Support\Facades\DB;
     use Illuminate\Support\Facades\Auth;
+
     $articles = DB::table('article')
         ->join('category', 'article.category_id', '=', 'category.category_id')
         ->select('article.*', 'category.name')
         ->where('article.is_active', 1)
         ->get();
-  
+
     ?>
     <header>
 
@@ -155,15 +155,15 @@
                         Specialized in <abbr title="Accessibility">PHP</abbr>
                         and Core Web Vitals
                     </p>
-                   
+
                     <div class="btn-group">
-                    @csrf 
-                    @if(Auth::guest())
-                    <a href="{{url('login')}}" class="btn btn-secondary"> Login</a>
-                    @else
-                            <a href="Blog/Create" class="btn btn-secondary">Add Blog</a>
-                            <a href="{{url('logout')}}" class="btn btn-secondary"> Logout</a>
-                    @endif
+                        @csrf
+                        @if(Auth::user())
+                        <a href="Blog/Create" class="btn btn-secondary">Add Blog</a>
+                        <a href="{{url('logout')}}" class="btn btn-secondary"> Logout</a>
+                        @else
+                        <a href="{{url('login')}}" class="btn btn-secondary"> Login</a>
+                        @endif
                     </div>
 
                 </div>
@@ -197,56 +197,52 @@
                     <div class="blog-card-group">
                         @csrf
                         @foreach ($articles as $article)
-                            <div class="blog-card">
-                                <div class="blog-card-banner">
-                                    <img src="{{url("./assets/images/$article->img")}}" width="250"
-                                        class="blog-banner-img">
-                                </div>
+                        <div class="blog-card">
+                            <div class="blog-card-banner">
+                                <img src="{{url("./assets/images/$article->img")}}" width="250" class="blog-banner-img">
+                            </div>
 
-                                <div class="blog-content-wrapper">
+                            <div class="blog-content-wrapper">
 
-                                    <button class="blog-topic text-tiny">{{ $article->name }}</button>
+                                <button class="blog-topic text-tiny">{{ $article->name }}</button>
 
-                                    <h3>
-                                        <a href="post/{{ $article->article_id }}" class="h3">
-                                            {{ $article->title }}
-                                        </a>
-                                    </h3>
+                                <h3>
+                                    <a href="post/{{ $article->article_id }}" class="h3">
+                                        {{ $article->title }}
+                                    </a>
+                                </h3>
 
-                                    <p class="blog-text">
-                                        {{ $article->article_body }}
-                                    </p>
+                                <p class="blog-text">
+                                    {{ $article->article_body }}
+                                </p>
 
-                                    <div class="wrapper-flex">
+                                <div class="wrapper-flex">
 
-                                        <div class="profile-wrapper">
-                                            <img src="./assets/images/author.png" alt="Noureldin Farag" width="50">
-                                        </div>
+                                    <div class="profile-wrapper">
+                                        <img src="./assets/images/author.png" alt="Noureldin Farag" width="50">
+                                    </div>
 
-                                        <div class="wrapper">
-                                            <a href="#" class="h4">Noureldin Farag</a>
-
-                                        </div>
+                                    <div class="wrapper">
+                                        <a href="#" class="h4">Noureldin Farag</a>
 
                                     </div>
-                                    @csrf
-                                   
-                                    @if(Auth::guest())
-                                    <div></div>
-                                    @else
-                                        <div class="flex-action">
-                                            <a href="{{ url("post/delete/ $article->article_id") }}"><i
-                                                    class="fa-solid fa-trash"></i></a>
-                                            <a href="{{ url("post/hide/$article->article_id") }}">
-                                                <i class="fa-solid fa-eye-slash"></i></a>
-                                            <a href="edit/{{ $article->article_id }}">
-                                                <i class="fa-solid fa-pen-to-square"></i>
-                                            </a>
-                                        </div>
-                                    @endif
 
                                 </div>
+                                @csrf
+
+                                @if(Auth::user())
+                                <div class="flex-action">
+                                    <a href="{{ url("post/delete/ $article->article_id") }}"><i class="fa-solid fa-trash"></i></a>
+                                    <a href="{{ url("post/hide/$article->article_id") }}">
+                                        <i class="fa-solid fa-eye-slash"></i></a>
+                                    <a href="edit/{{ $article->article_id }}">
+                                        <i class="fa-solid fa-pen-to-square"></i>
+                                    </a>
+                                </div>
+                                @endif
+
                             </div>
+                        </div>
                         @endforeach
 
                     </div>
@@ -254,10 +250,6 @@
                     <button style="display:none" class="btn load-more">Load More</button>
 
                 </div>
-
-
-
-
 
                 <!--
           - ASIDE
@@ -346,8 +338,7 @@
             <div class="wrapper">
 
                 <a href="#" class="footer-logo">
-                    <img src="./assets/images/logo-light.svg" alt="DevBlog's Logo" width="150"
-                        class="logo-light">
+                    <img src="./assets/images/logo-light.svg" alt="DevBlog's Logo" width="150" class="logo-light">
                     <img src="./assets/images/logo-dark.svg" alt="DevBlog's Logo" width="150" class="logo-dark">
                 </a>
 
